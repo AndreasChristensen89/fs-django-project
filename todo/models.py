fd36171dee880056b04e63e64ef271313f2564d5
AndreasChristensen89 <brogaard89@gmail.com>
2021-09-15 14:10:39 +0000

    Add things

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.charField(max_length=50, null=False, blank=False)
+    done = models.booleanField(max_length=50, null=False, blank=False, default=False))
