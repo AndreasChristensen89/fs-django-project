b14238dc688debbef79da26b29c338258ef07edb
AndreasChristensen89 <brogaard89@gmail.com>
2021-09-16 09:59:42 +0000

    Add full CRUD functionality

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Item(models.Model):
-    name = models.charField(max_length=50, null=False, blank=False)
-    done = models.booleanField(max_length=50, null=False, blank=False, default=False))
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(max_length=50, null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
